240cae69ee3f34890cdd42042869b4e9d631f5d2
aditya17paul <144152506+aditya17paul@users.noreply.github.com>
2026-03-10 17:05:25 +0000

    setup

--- movie/app/index.tsx
+++ movie/app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center bg">
+    <View className="flex-1 justify-center items-center">
       <Text>Welcome to movie app</Text>
     </View>
   );
